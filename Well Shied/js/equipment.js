476c4cdce42db3ff3acd0759e4e42966dc3a7727
outpjj <2546773918@qq.com>
2024-04-08 13:02:29 +0800

    完善添加设备弹窗提示

--- Well Shied/js/equipment.js
+++ Well Shied/js/equipment.js
@@ -1,8 +1,34 @@
-// 获取按钮元素
-var button = document.getElementById('add_btn');
 
+// 为按钮添加点击事件监听器 alert('检测到摄像头是否添加该设备');
+
+// 获取按钮和弹框元素
+var button = document.getElementById('add_btn');
+var modal = document.getElementById('myModal');
+var span = document.getElementsByClassName("close")[0];
+var yesbut = document.getElementById('yes-but');
+var nobut = document.getElementById('no-but');
 // 为按钮添加点击事件监听器
 button.addEventListener('click', function() {
-    // 当按钮被点击时，显示一个弹框
-    alert('是否添加该设备');
+
+    alert('检测到摄像头是否添加该设备');
+    modal.style.display = "block";
+});
+
+// 为关闭按钮添加点击事件监听器
+span.addEventListener('click', function() {
+    modal.style.display = "none";
+});
+yesbut.addEventListener('click', function() {
+    modal.style.display = "none";
+
+});
+nobut.addEventListener('click', function() {
+    modal.style.display = "none";
+});
+
+// 当用户点击弹框以外的区域时关闭弹框
+window.addEventListener('click', function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 });
